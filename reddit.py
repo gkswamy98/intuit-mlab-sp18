cb36049717a8a8f549240bbbb6823067d1492068
Matthew Trepte <matthewtrepte@Matthews-MacBook-Pro-2.local>
2018-03-04 14:35:32 -0800

    reddit scraper

---
+++ reddit.py
@@ -0,0 +1,47 @@
+import praw
+import json
+import datetime
+import time
+reddit = praw.Reddit(client_id='tJPMCPv9rwNJrA',
+					 client_secret='5rcOdSXgSSBfV6iAOaz3PhPN4Lo',
+					 user_agent='proseccodaddy')
+
+subreddit = reddit.subreddit('personalfinance')
+
+file_name = 'personalfinance.json'
+open(file_name, 'w').close()
+
+count = 0
+list_dicts = []
+
+for year in range(2016, 2017):
+	for month in range(1,13):
+
+		startDate = datetime.date(year, month, 1)
+		endDate = datetime.date(year, (month + 1) % 12, 1)
+
+		startUnix = time.mktime(startDate.timetuple())
+		endUnix = time.mktime(endDate.timetuple())
+
+		for submission in subreddit.submissions(startUnix, endUnix):
+
+			comment_bodies = ''
+			comments = list(submission.comments)
+			for comment in comments:
+				try:
+					comment_bodies += comment.body.encode('utf-8') + ' '
+				except:
+					pass
+
+			sub_dict = {'title': submission.title.encode('utf-8'),
+						'flair': submission.title.encode('utf-8'),
+						'upvotes': submission.score,
+						'body': submission.selftext.encode('utf-8'),
+						'date': startUnix,
+						'comments': comment_bodies}
+			list_dicts.append(sub_dict)
+			count += 1
+
+file = open(file_name,'w')
+file.write(json.dumps(list_dicts))
+file.close()
